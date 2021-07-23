170ceaccc7244f64e4badf37eefe4c61750fc5f3
Stephen Beiter <stephenbeiter@gmail.com>
2021-07-23 11:34:58 -0700

    replaced api keys

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51ISUv2LaWzEWwDGjaxGFwQBRoiGK2LcpyVQ2mNgbXCvQnI0qePqp1LKI0uXBelTgr36p0VLbjvHJsUyhhlSCVTou00xKZrPdvi';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBvxJgH1u5AKPPBB_hSqZcruDbBxNcF7GI",
+  authDomain: "crwn-db-6ebc2.firebaseapp.com",
+  projectId: "crwn-db-6ebc2",
+  storageBucket: "crwn-db-6ebc2.appspot.com",
+  messagingSenderId: "319218637170",
+  appId: "1:319218637170:web:650b7e577956394e9ea5eb",
+  measurementId: "G-K48FMQD01S"
 };
 
 firebase.initializeApp(config);
